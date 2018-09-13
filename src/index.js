6c8981eaa53a295255af5ef7ba262c8def539280
veterkam <veterkam@gmail.com>
2018-09-13 17:08:14 +0300

    quadratic-equation/feature:  implement solveEquation function, wihch solves Quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  // equation example: a * x^2 + b * x + c
+  // extract elements a, b, c
+  let elements = equation.split(' ');
+  let a = +elements[0];
+  let signB = elements[3];
+  let b = +elements[4];
+  let signC = elements[7];
+  let c = +elements[8]; 
+
+  if(signB == '-') b = -b;
+  if(signC == '-') c = -c;
+  
+  // solve equation
+  let D = b * b - 4 * a * c;
+  if(D > 0) {
+    sqrtD = Math.sqrt(D);
+    let x1 = (-b - sqrtD) / (2 * a);
+    let x2 = (-b + sqrtD) / (2 * a);
+    return [ Math.round(x1), Math.round(x2) ].sort( (a, b) => a-b );
+  }
+
+  return null;
 }
